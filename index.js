6cfcaaff0d17848a00e55528b2131bc3bffd5db9
Luana Furtado <luana.furtado@calbrightcollege.org>
2026-01-07 21:03:14 -0800

    ocumenting what each main section of the code is doing with comments.

--- index.js
+++ index.js
@@ -5,30 +5,34 @@ import books from "./data/books.js";
 const app = express();
 const PORT = 3000;
 
+// Middleware that allows the app to read JSON data sent in requests
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Book API is running");
+    res.send("Book API is running");
 });
-
+// Return all books from the hardcoded data file
 app.get("/books", (req, res) => {
     res.json(books);
-    });
+});
 
 app.get("/books/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const book = books.find(book => book.id === id);
-  if (book) {
-    res.json(book);
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
+    // Get the id from the URL and convert it to a number
+    const id = Number(req.params.id);
+    // Find the book that matches the given id
+    const book = books.find(book => book.id === id);
+    if (book) {
+        res.json(book);
+    } else {
+        // Send a 404 error if the book does not exist
+        res.status(404).json({ message: "Book not found" });
+    }
 });
 
 app.post("/books", (req, res) => {
     const newBook = {
         id: books.length + 1,
-        ...req.body
+        ...req.body // Add all properties from req.body (sent by the client) to the new book
     };
 
     books.push(newBook);
@@ -36,5 +40,5 @@ app.post("/books", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
