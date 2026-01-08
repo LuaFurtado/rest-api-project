c58c104ea8010f9a5e6beb973fc86ee643348847
Luana Furtado <luana.furtado@calbrightcollege.org>
2026-01-07 17:46:50 -0800

    add remaining book placeholders

--- data/books.js
+++ data/books.js
@@ -8,7 +8,118 @@ const books = [
     publicDomain: true,
     description: "",
     downloadUrl: ""
+  },
+    {
+    id: 2,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 3,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 4,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 5,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 6,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 7,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 8,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 9,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 10,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 11,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  },
+  {
+    id: 12,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
   }
+
 ];
 
 module.exports = books;
