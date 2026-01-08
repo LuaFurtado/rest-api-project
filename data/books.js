7dd14efab1570454accab4c0f2bc60d00af47ffd
Luana Furtado <luana.furtado@calbrightcollege.org>
2026-01-07 19:05:02 -0800

    added first book data

--- data/books.js
+++ data/books.js
@@ -1,14 +1,17 @@
 const books = [
   {
-    id: 1,
-    title: "",
-    author: "",
-    year: "",
-    language: "pt-BR",
-    publicDomain: true,
-    description: "",
-    downloadUrl: ""
-  },
+  id: 1,
+  title: "Turma da Mônica: Água boa para beber",
+  author: "Maurício de Souza",
+  year: 2002,
+  language: "pt-BR",
+  publicDomain: false,
+  description: "Educational digital comic book about the importance of drinking clean water.",
+  assets: {
+    coverImage: "/assets/covers/book-1-cover.png",
+    pdf: "/assets/books/book-1.pdf"
+  }
+    },
     {
     id: 2,
     title: "",
