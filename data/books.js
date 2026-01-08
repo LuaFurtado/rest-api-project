6bada1ee0ed8f1b99592f05b74144383b54e07d3
Luana Furtado <luana.furtado@calbrightcollege.org>
2026-01-07 17:45:21 -0800

    add base book data structure

--- data/books.js
+++ data/books.js
@@ -1,1 +1,15 @@
-// books data will be added here
+const books = [
+  {
+    id: 1,
+    title: "",
+    author: "",
+    year: "",
+    language: "pt-BR",
+    publicDomain: true,
+    description: "",
+    downloadUrl: ""
+  }
+];
+
+module.exports = books;
+
